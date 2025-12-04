1cb12f23437d239567b6ad95612cde63097ad45e
danninx <public.danninx@gmail.com>
2025-12-03 23:22:39 -0600

    move monitor assertions into config

--- modules/home-manager/hyprflake/config/monitor.nix
+++ modules/home-manager/hyprflake/config/monitor.nix
@@ -28,32 +28,32 @@
       )
     ];
 in {
-  assertions = [
-    {
-      assertion = let
-        scales = lib.mapAttrsToList (name: value: value.scale) cfg.monitors;
-      in
-        lib.lists.all (scale: scale > 0.0) scales;
-      message = "The scale of a monitor must be positive number";
-    }
-    {
-      assertion = let
-        primaries = lib.filterAttrs (n: v: v.primary == true) cfg.monitors;
-      in (lib.length (lib.attrNames primaries) == 1);
-      message = "You must set exactly one primary monitor";
-    }
-    {
-      assertion = let
-        defaultSpaces = lib.filterAttrs (n: v: v.default == true && v.monitor != null) cfg.workspaces;
-        monitorOccurences = lib.mapAttrsToList (workspace: val: val.monitor) defaultSpaces;
-        valid = lib.length monitorOccurences == lib.length (lib.unique monitorOccurences);
-      in
-        valid;
-      message = "Monitors may have at most one default workspace";
-    }
-  ];
-
   config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = let
+          scales = lib.mapAttrsToList (name: value: value.scale) cfg.monitors;
+        in
+          lib.lists.all (scale: scale > 0.0) scales;
+        message = "The scale of a monitor must be positive number";
+      }
+      {
+        assertion = let
+          primaries = lib.filterAttrs (n: v: v.primary == true) cfg.monitors;
+        in (lib.length (lib.attrNames primaries) == 1);
+        message = "You must set exactly one primary monitor";
+      }
+      {
+        assertion = let
+          defaultSpaces = lib.filterAttrs (n: v: v.default == true && v.monitor != null) cfg.workspaces;
+          monitorOccurences = lib.mapAttrsToList (workspace: val: val.monitor) defaultSpaces;
+          valid = lib.length monitorOccurences == lib.length (lib.unique monitorOccurences);
+        in
+          valid;
+        message = "Monitors may have at most one default workspace";
+      }
+    ];
+
     wayland.windowManager.hyprland.settings.monitor = lib.mapAttrsToList mkMonitor cfg.monitors;
   };
 }
