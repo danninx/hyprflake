5818e7577ae1f023f79d774f4ea6022014b9c92c
danninx <public.danninx@gmail.com>
2025-12-03 23:26:41 -0600

    nullOr is part of lib.types

--- modules/home-manager/hyprflake/types/workspace.nix
+++ modules/home-manager/hyprflake/types/workspace.nix
@@ -2,7 +2,7 @@
 lib.types.submodule {
   options = {
     monitor = lib.mkOption {
-      type = lib.nullOr lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       default = null;
       description = "Monitor to bind this workspace to. Monitor will not be bound if null.";
       example = "HDMI-A-1";
@@ -16,21 +16,21 @@ lib.types.submodule {
     };
 
     gapsIn = lib.mkOption {
-      type = lib.nullOr lib.types.int;
+      type = lib.types.nullOr lib.types.int;
       default = null;
       description = "Sets the gaps between windows, in pixels, for this workspace. Ignored if null.";
       example = 2;
     };
 
     gapsOut = lib.mkOption {
-      type = lib.nullOr lib.types.int;
+      type = lib.types.nullOr lib.types.int;
       default = null;
       description = "Sets the gaps between windows and monitor edges, in pixels, for this workspace. Ignored if null.";
       example = 5;
     };
 
     borderSize = lib.mkOption {
-      type = lib.nullOr lib.types.int;
+      type = lib.types.nullOr lib.types.int;
       default = null;
       description = "Sets the border size around windows, in pixels, for this workspace. Ignored if null.";
       example = 1;
